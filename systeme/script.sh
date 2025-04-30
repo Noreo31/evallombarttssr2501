146bf93005bba22215e0a47f1a1cf2e3bdc4d6c9
Noreo31 <slombart@live.fr>
2025-04-30 23:10:15 +0200

    mon projet

--- systeme/script.sh
+++ systeme/script.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-echo "Où voulez-vous enregistrer le projet ? " "C:\Users\Steph\Desktop\evallombarttssr2501"
+echo "Où voulez-vous enregistrer le projet ?"
 read directory
 mkdir $directory
 cd $directory
@@ -7,6 +6,6 @@ echo "Quel est le nom de votre projet ?"
 read project
 mkdir $project
 cd $project
-touch $index.html $style.css $README.md
-echo "Le projet a été ajouté avec succés."
+touch index.html style.css README.md
+echo "Le projet a été ajouté avec succés"
 
